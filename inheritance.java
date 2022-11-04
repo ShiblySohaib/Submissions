9b9d057f518b7bd3c4b712b82bf710b8f1dcf3eb
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-11-04 21:39:59 +0600

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -4,12 +4,14 @@
 
 
 
-                public class parent {
+                public class parent{
                     String sentence="parent attribute.";
                     void print(){
                         System.out.println("parent method.");
                     }
                 }
+
+
 
                 class child extends parent{
                     public static void main(String[]args){
@@ -23,23 +25,39 @@
 
 
 
-                                                                                              
-                public class parent {                                               public class parent{
-                    String sentence="parent attribute.";                                 String sentence="parent_2 attribute.";
-                    void print(){                                                        void print(){
-                        System.out.println("parent method.");                                System.out.println("parent_2 method.);
-                    }                                                                    }
-                }                                                                   }
 
-                                                                                                                
-                                                                                                                
-                                          class child extends parent,parent_2{
-                                              public static void main(String[]args){
-                                                  child obj=new child();
-                                                  System.out.println(obj.sentence);
-                                                  obj.print();
-                                              }
-                                          }
+
+                                                                                              
+                public class parent{
+                    String sentence="parent attribute.";
+                    void print(){
+                        System.out.println("parent method.");
+                    }
+                }
+
+
+
+
+
+                public class parent_2{
+                    String sentence="parent attribute.";
+                    void print(){
+                        System.out.println("parent method.");
+                    }
+                }
+
+
+
+
+
+                class child extends parent,parent_2{
+                    public static void main(String[]args){
+                        child obj=new child();
+                        System.out.println(obj.sentence);
+                        obj.print();
+                    }
+                }
+
                                                                                                                 
                                                                                                                 
                                                                                                                 
